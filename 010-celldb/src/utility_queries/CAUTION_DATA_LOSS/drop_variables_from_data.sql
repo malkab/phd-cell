27a1310c5b8d06a034db5d8fdc0bf3289ca0b275
Juan Pedro Pérez Alcántara <jp.perez.alcantara@gmail.com>
2021-11-26 09:50:46 +0100

    feat(added a new utility query to drop variables from data)

---
+++ 010-celldb/src/utility_queries/CAUTION_DATA_LOSS/drop_variables_from_data.sql
@@ -0,0 +1,26 @@
+/**
+
+  Drops variables from cell_data.data data column. BEWARE!!!
+
+*/
+
+update cell_data.data set data = data - '0721'  where data ? '0721'  ;
+update cell_data.data set data = data - '148a'  where data ? '148a'  ;
+update cell_data.data set data = data - '27c6'  where data ? '27c6'  ;
+update cell_data.data set data = data - '3374'  where data ? '3374'  ;
+update cell_data.data set data = data - '3a21'  where data ? '3a21'  ;
+update cell_data.data set data = data - '568cb' where data ? '568cb' ;
+update cell_data.data set data = data - '57ca'  where data ? '57ca'  ;
+update cell_data.data set data = data - '618e'  where data ? '618e'  ;
+update cell_data.data set data = data - '9149'  where data ? '9149'  ;
+update cell_data.data set data = data - '924a'  where data ? '924a'  ;
+update cell_data.data set data = data - 'ad6f'  where data ? 'ad6f'  ;
+update cell_data.data set data = data - 'b76f0' where data ? 'b76f0' ;
+update cell_data.data set data = data - 'dc0f'  where data ? 'dc0f'  ;
+update cell_data.data set data = data - 'dc53'  where data ? 'dc53'  ;
+update cell_data.data set data = data - 'e248'  where data ? 'e248'  ;
+update cell_data.data set data = data - 'f0b1'  where data ? 'f0b1'  ;
+update cell_data.data set data = data - 'f414'  where data ? 'f414'  ;
+update cell_data.data set data = data - 'f64c'  where data ? 'f64c'  ;
+update cell_data.data set data = data - 'fbe8'  where data ? 'fbe8'  ;
+update cell_data.data set data = data - 'fe49'  where data ? 'fe49'  ;
